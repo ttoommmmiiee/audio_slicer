97fbcce9bdcd6670bcb5953e03aa434b2645845b
ttoommmmiiee <tommie@blackshuck.co>
2023-06-02 13:26:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='audioslicer',
+    name='tommieaudioslicer',
     version='1.0.0',
     description='SimpleSlicer',
     author='Tommie Introna',
