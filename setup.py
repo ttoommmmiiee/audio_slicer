84779abf3135b5b12679a6d0f945664bd36b97ef
ttoommmmiiee <tommie@blackshuck.co>
2023-06-02 13:02:08 +0100

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='audio_slicer',
+    version='1.0.0',
+    description='SimpleSlicer',
+    author='Tommie Introna',
+    author_email='tommie@blackshuck.co',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'soundfile',
+        'random',
+        'os',
+    ],
+)
